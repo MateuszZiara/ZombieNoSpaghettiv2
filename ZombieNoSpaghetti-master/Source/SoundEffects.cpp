89f351153c6a423968c20c9b0e54737c33212560
Paweł <karpawell@gmail.com>
2024-01-07 01:22:25 +0100

    Dokumentacja - poprawki

--- ZombieNoSpaghetti-master/Source/SoundEffects.cpp
+++ ZombieNoSpaghetti-master/Source/SoundEffects.cpp
@@ -1,17 +1,33 @@
-#include <SFML/Audio.hpp>
+/**
+ * @file SoundEffects.cpp
+ * @brief Implementacja klasy SoundEffects.
+ */
+
 #include "../Header/SoundEffects.h"
 #include <iostream>
 
+/**
+ * @brief Konstruktor domyślny klasy SoundEffects.
+ */
 SoundEffects::SoundEffects() {
     addToContainer(1, "../Sounds/background.ogg");
     //addToContainer(2, "../Sounds/zombieNoise.ogg");
     //addToContainer(3, "../Sounds/playerNoise.ogg");
 }
 
+/**
+ * @brief Dodaje efekt dźwiękowy do kontenera.
+ * @param key Klucz przypisujący efekt dźwiękowy.
+ * @param value Ścieżka do pliku efektu dźwiękowego.
+ */
 void SoundEffects::addToContainer(int key, const std::string& value) {
     soundsContainer[key] = value;
 }
 
+/**
+ * @brief Odtwarza muzykę na podstawie podanego klucza.
+ * @param value Klucz związany z pożądaną muzyką.
+ */
 void SoundEffects::playMusic(int value) {
     if (music.getStatus() != sf::Music::Playing) {
         auto it = soundsContainer.find(value);
@@ -28,19 +44,26 @@ void SoundEffects::playMusic(int value) {
     }
 }
 
+/**
+ * @brief Zatrzymuje aktualnie odtwarzaną muzykę.
+ */
 void SoundEffects::stopMusic() {
     music.stop();
 }
 
+/**
+ * @brief Odtwarza efekt dźwiękowy na podstawie podanego klucza.
+ * @param value Klucz związany z pożądanym efektem dźwiękowym.
+ */
 void SoundEffects::makeSound(int value) {
     auto it = soundsContainer.find(value);
     if (it != soundsContainer.end()) {
         if (!music.openFromFile(it->second)) {
-            std::cout<<"Nie znaleziono utworu.";
+            std::cout << "Nie znaleziono utworu.";
         }
         music.setVolume(20);
         music.play();
     } else {
-        std::cout<<"Podana wartość nie jest zadeklarowana w słowniku.";
+        std::cout << "Podana wartość nie jest zadeklarowana w słowniku.";
     }
 }
